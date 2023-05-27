18198be921db9b350c96afa1c337d145de7fbcbe
Hederino <hederino@mail.com>
2023-05-27 15:51:18 +0300

    Update

---
+++ main.py
@@ -0,0 +1,47 @@
+from note import Note, note_a4
+from message import Message
+
+A4 = 440.0
+A4_MAX, A4_MIN = 500.0, 400.0
+
+def main(freq_a4=A4):
+
+    print(Message.welcome)
+    print(Message.info(note_a4, freq_a4))
+
+    while True:
+        
+        i = input(Message.enter_freq).strip().lower()
+        
+        if i == "x":
+            print(Message.bye)
+            break
+        
+        if i == "f":
+            while True:
+                f = input(Message.prompt_a4(note_a4, freq_a4)).strip().lower()
+                if f == "x":
+                    print(Message.back_to_main)
+                    break
+                try:
+                    new_a4 = float(f)
+                    if A4_MIN <= new_a4 <= A4_MAX:
+                        print(Message.a4_set(note_a4, freq_a4, new_a4))
+                        freq_a4 = new_a4
+                        break
+                    print(Message.a4_out_of_range(note_a4, A4_MIN, A4_MAX))
+                except ValueError:
+                    print(Message.inv_freq)
+
+        else:
+            try:
+                freq = float(i)
+                note, cents_diff = Note.freq_to_note(freq, freq_a4)
+                note_0_cents_freq = note.note_to_freq(freq_a4)
+                print(Message.closest(freq, note, note_0_cents_freq, cents_diff))
+            except ValueError:
+                print(Message.inv_freq)
+
+
+if __name__ == "__main__":
+    main()
---
+++ note.py
@@ -0,0 +1,59 @@
+from math import log2
+
+class Note:
+    NOTE_NAMES = 'C', 'D', 'E', 'F', 'G', 'A', 'B'
+    SUB = str.maketrans("-0123456789", "₋₀₁₂₃₄₅₆₇₈₉")
+    NOTE_STEPS = 0, 2, 4, 5, 7, 9, 11
+    ACCIDENTALS = {-1: "♭", 0: "", 1: "♯"}
+
+    def __init__(self, note_index: int, octave: int, accidental=0):
+        self.note_index = note_index
+        self.octave = octave
+        self.accidental = accidental
+
+    def __str__(self):
+        accidental = f"{self.ACCIDENTALS[self.accidental]}"
+        octave = f"{str(self.octave).translate(self.SUB)}"
+        return ''.join([self.note_name, accidental, octave])
+    
+    @property
+    def note_name(self):
+        return self.NOTE_NAMES[self.note_index]
+    
+    @property
+    def is_natural(self):
+        return self.accidental == 0
+    
+    @property
+    def semitone_value(self):
+        return 12 * (self.octave + 1) + self.NOTE_STEPS[self.note_index] + self.accidental
+
+    def enharmonic_note(self):
+        if self.is_natural:
+            return self
+        return self.note_from_semitone_value(self.semitone_value, self.accidental == 1)
+
+    @classmethod
+    def note_from_semitone_value(cls, n, flat=False):
+        octave, semitones, acc = *divmod(n, 12), 0
+        octave -= 1
+        if semitones not in cls.NOTE_STEPS:
+            if flat:
+                semitones, acc = semitones + 1, acc - 1
+            else:
+                semitones, acc = semitones - 1, acc + 1
+        index = cls.NOTE_STEPS.index(semitones)
+        return Note(index, octave, acc)
+    
+    @classmethod
+    def freq_to_note(cls, freq, a4=440.0):
+        cents_from_a4 = 1200 * log2(freq / a4)
+        semitones = round(cents_from_a4 / 100)
+        cents = cents_from_a4 - 100 * semitones
+        note = cls.note_from_semitone_value(69 + semitones)
+        return note, round(cents, 2)
+    
+    def note_to_freq(self, a4=440.0):
+        return round(a4 * 2 ** ((self.semitone_value - 69) / 12), 3)
+    
+note_a4 = Note(5, 4)
